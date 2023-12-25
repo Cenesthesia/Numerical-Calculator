12d2f9acd22d801c6cc2aeaa6bb82cf29b102d2f
Cenesthesia <frolkinpasha2004@gmail.com>
2023-12-25 23:27:30 +0300

    Implemented the main user window for data entry

--- numericalcalculator/src/main/java/com/curswork/numericalcalculator/NumericalCalculator.java
+++ numericalcalculator/src/main/java/com/curswork/numericalcalculator/NumericalCalculator.java
@@ -4,9 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * JavaFX App
@@ -17,8 +20,12 @@ public class NumericalCalculator extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("/numericalcalculator/primary"));
         stage.setScene(scene);
+        stage.setTitle("Numercal Calculator");
+        stage.setMinHeight(540);
+        stage.setMinWidth(870);
+        stage.getIcons().add(new Image(NumericalCalculator.class.getResourceAsStream("/numericalcalculator/calculator.png")));
         stage.show();
     }
 
--- numericalcalculator/src/main/java/com/curswork/numericalcalculator/PrimaryController.java
+++ numericalcalculator/src/main/java/com/curswork/numericalcalculator/PrimaryController.java
@@ -1,12 +1,63 @@
 package com.curswork.numericalcalculator;
 
-import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        
+    private TextField accuraty;
+
+    @FXML
+    private RadioButton chordRad;
+
+    @FXML
+    private Button decideBut;
+
+    @FXML
+    private RadioButton dichotomyRad;
+
+    @FXML
+    private TextField expression;
+
+    @FXML
+    private LineChart<?, ?> functionGraph;
+
+    @FXML
+    private Button helpButton;
+
+    @FXML
+    private TextField leftBorder;
+
+    @FXML
+    private TextField rightBorder;
+
+    @FXML
+    private ToggleGroup usingMethod;
+
+    @FXML
+    void decideExpression(ActionEvent event) {
+
     }
+
+    @FXML
+    void setChordMethod(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setDichotomyMethod(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showHelp(ActionEvent event) {
+
+    }
+
 }
